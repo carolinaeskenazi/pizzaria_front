3e5fa23441e35055e2b09ce0e82937277e5f41af
Thaisrs165 <thaisrs3@al.insper.edu.br>
2025-05-06 15:40:43 -0300

    pizzas no menu

--- pizzaria-front/src/App.jsx
+++ pizzaria-front/src/App.jsx
@@ -1,22 +1,38 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import { ClienteForm } from './ClienteForm'
 import { ClienteTable } from './ClienteTable'
+import { PizzaForm } from './PizzaForm'
+import { PizzaTable } from './PizzaTable'
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <nav>
-        <Link to="/cadastrarCliente">Cadastrar Cliente</Link>
-        <Link to="/listarClientes">Listar Clientes</Link>
-      </nav>
+    <>
+      <div className="App">
+        <nav>
+          <h2>Clientes</h2>
+          <Link to="/cadastrarCliente">Cadastrar Cliente</Link>
+          <Link to="/listarClientes">Listar Clientes</Link>
+          
+          <h2>Pizzas</h2>
+          <Link to="/cadastrarPizza">Cadastrar Pizza</Link>
+          <Link to="/listarPizzas">Listar Pizzas</Link>
+        </nav>
 
-      <Routes>
-        <Route path="/" element={<ClienteForm />} />
-        <Route path="/cadastrarCliente" element={<ClienteForm />} />
-        <Route path="/listarClientes" element={<ClienteTable />} />
-      </Routes>
-    </div>
+        <Routes>
+          {/* Rotas de Cliente */}
+          <Route path="/cadastrarCliente" element={<ClienteForm />} />
+          <Route path="/listarClientes" element={<ClienteTable />} />
+          
+          {/* Rotas de Pizza */}
+          <Route path="/cadastrarPizza" element={<PizzaForm />} />
+          <Route path="/listarPizzas" element={<PizzaTable />} />
+
+          {/* Rota padrão (opcional) */}
+          <Route path="/" element={<h1>Bem-vindo ao Sistema de Pizzaria</h1>} />
+        </Routes>
+      </div>
+    </>
   )
 }
 
